839331564363993700c17e4a8938cb6fcb7cd689
Shahzaib Akash <shahzaibakash@gmail.com>
2019-12-21 16:22:52 -0500

    enums

---
+++ src/enums.rs
@@ -0,0 +1,34 @@
+// Types which have few definite types
+
+
+enum  Movement {
+    // Variables
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+fn move_avatar(m: Movement) {
+    // Perform Action depending on info
+    match m {
+        Movement::Up => println!("Avatar Moving Up"),
+        Movement::Down => println!("Avatar Moving Down"),
+        Movement::Left => println!("Avatar Moving Left"),
+        Movement::Right => println!("Avatar Moving Right")
+    }
+}
+
+
+pub fn run(){
+    let avatar1 = Movement::Left;
+    let avatar2 = Movement::Up;
+    let avatar3 = Movement::Down;
+    let avatar4 = Movement::Right;
+
+    move_avatar(avatar1);
+    move_avatar(avatar2);
+    move_avatar(avatar3);
+    move_avatar(avatar4);
+
+}
